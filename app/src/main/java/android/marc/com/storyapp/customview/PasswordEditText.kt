aae89de5e8bfb6316c02e0bc168edaf3a487e10a
Mario Claudius <mcmario7901@gmail.com>
2023-01-15 17:52:47 +0700

    create custom edittext for password edittext

---
+++ app/src/main/java/android/marc/com/storyapp/customview/PasswordEditText.kt
@@ -0,0 +1,63 @@
+package android.marc.com.storyapp.customview
+
+import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.util.Log
+import androidx.appcompat.widget.AppCompatEditText
+import kotlinx.coroutines.*
+import java.util.*
+
+class PasswordEditText: AppCompatEditText {
+
+    private var timer: Timer? = null
+    private var viewModelJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    override fun addTextChangedListener(watcher: TextWatcher?) {
+        super.addTextChangedListener(watcher)
+    }
+
+    private fun init() {
+        this.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (timer != null) {
+                    timer?.cancel()
+                }
+            }
+
+            override fun afterTextChanged(text: Editable?) {
+                timer = Timer()
+                timer?.schedule(object : TimerTask() {
+                    override fun run() {
+                        if (!text.isNullOrEmpty()) {
+                            if (text.length < 8) {
+                                uiScope.launch {
+                                    withContext(Dispatchers.Main) {
+                                        setError("Password must not less than 8 characters")
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                }, 1000)
+            }
+        })
+    }
+}
---
+++ app/src/main/java/android/marc/com/storyapp/login/LoginActivity.kt
@@ -0,0 +1,34 @@
+package android.marc.com.storyapp.login
+
+import android.marc.com.storyapp.databinding.ActivityLoginBinding
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        hideActionBar()
+    }
+
+    private fun hideActionBar() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+}
