ce177069fbb4fe499b15e80c715086a97412d1e9
Mario Claudius <mcmario7901@gmail.com>
2023-01-30 13:11:13 +0700

    resolve closeSoftKeyboard() ambiguous

--- app/src/androidTest/java/android/marc/com/storyapp/activity/login/LoginActivityTest.kt
+++ app/src/androidTest/java/android/marc/com/storyapp/activity/login/LoginActivityTest.kt
@@ -7,7 +7,8 @@ import android.marc.com.storyapp.activity.main.MainActivity
 import android.marc.com.storyapp.api.ApiConfig
 import androidx.test.espresso.Espresso.*
 import androidx.test.espresso.IdlingRegistry
-import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
@@ -17,7 +18,6 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import okhttp3.internal.wait
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
